cf9d358b115e67b12e884bb17835f5d44abeeb53
Szymon Auguścik <48166468+SzymonAuguscik@users.noreply.github.com>
2020-05-14 16:45:42 +0200

    Rename code10.m to Lab10/code10.m

--- Lab10/code10.m
+++ Lab10/code10.m
@@ -3,7 +3,7 @@ clear; clc;
 G1 = zpk([], [-1 -1 -1], 10);
 G2 = zpk([], -2, 3);
 
-%Wartoœæ ustalona
+%WartoÅ“Ã¦ ustalona
 
 %sterowanie klasyczne
 R = pidstd(1,1);
@@ -19,29 +19,29 @@ R1 = pidtune(G1*sys_wew,R1,0.2);
 sys_zew = feedback(R1*G1*sys_wew,1);
 
 f = figure(1);
-f.Name = "Wartoœæ ustalona";
+f.Name = "WartoÅ“Ã¦ ustalona";
 figure(f)
 step(sys,sys_zew)
 legend("Ster. klasyczne","Ster. kaskadowe")
 
-%Zak³ócenia z1
+%ZakÂ³Ã³cenia z1
 
 Gz1kl = 1/(1+R*G1*G2);
 Gz1ka = (1+G2*R2)/(1+R2*G2+R1*R2*G2*G1);
 
 f = figure(2);
-f.Name = "Zak³ócenia z1";
+f.Name = "ZakÂ³Ã³cenia z1";
 figure(f)
 step(Gz1kl,Gz1ka)
 legend("Ster. klasyczne","Ster. kaskadowe")
 
-%Zak³ócenia z2
+%ZakÂ³Ã³cenia z2
 
 Gz2kl = G1/(1+R*G1*G2);
 Gz2ka = G1/(1+R2*G2+R1*R2*G2*G1);
 
 f = figure(3);
-f.Name = "Zak³ócenia z2";
+f.Name = "ZakÂ³Ã³cenia z2";
 figure(f)
 step(Gz2kl,Gz2ka)
 legend("Ster. klasyczne","Ster. kaskadowe")
